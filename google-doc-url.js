775031a8b363ec054cac228dd66b702e0b612358
yvsc <108088938+yvsc@users.noreply.github.com>
2022-06-23 12:48:40 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gtaCni2xMUju5Xf8P8PoTMXR9DBcfq1gC6UwjYqini4/edit#gid=0';
 
+
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
